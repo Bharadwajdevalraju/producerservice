2834c3168e73933fe44270ec4fb3b479b87cb5d2
Bharadwajdevalraju <51433505+Bharadwajdevalraju@users.noreply.github.com>
2020-01-22 12:36:10 +0530

    Update appconstant.clj

--- src/app/appconstant.clj
+++ src/app/appconstant.clj
@@ -5,7 +5,7 @@
 (def QUEUESTATUS_WAITING "WAITING")
 (def QUEUESTATUS_DONE "DONE")
 (def QUEUESTATUS_RUNNING "RUNNING")
-(def RESPONSE_ALREADYQUEUED "cannot queue this again")
+(def RESPONSE_ALREADYQUEUED "cannot queue this queue again")
 (def RESPONE_READYFORCONSUMPTION " queue is ready for consumption")
 
 
